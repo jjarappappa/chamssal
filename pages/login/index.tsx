1bfc6ed990bcbb82525cfeda6928054e7b8a3264
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-01 11:57:51 +0900

    FEAT :: input 수정 기능

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,8 +2,30 @@ import type { NextPage } from "next";
 import Header from "../../components/layout/header";
 import Head from "next/head";
 import styles from "../../styles/pages/login.module.scss";
+import { ChangeEvent, useState } from "react";
+
+interface loginInput {
+  email: string;
+  password: string;
+}
 
 const Login: NextPage = () => {
+  const [input, setInput] = useState<loginInput>({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    const nextInputs = {
+      ...input,
+      [name]: value,
+    };
+
+    setInput(nextInputs);
+  };
+
   return (
     <div>
       <Head>
@@ -14,14 +36,26 @@ const Login: NextPage = () => {
       <div className={styles.login}>
         <h1>로그인</h1>
         <div className={styles.inputs}>
-          <input type="text" placeholder="이메일" className={styles.input} />
+          <input
+            type="email"
+            placeholder="이메일"
+            className={styles.input}
+            value={input.email}
+            onChange={(e) => onChange(e)}
+            name="email"
+          />
           <input
             type="password"
             placeholder="비밀번호"
             className={styles.input}
+            value={input.password}
+            onChange={(e) => onChange(e)}
+            name="password"
           />
         </div>
-        <button className={styles.button}>로그인</button>
+        <button className={styles.button} onClick={() => console.log(input)}>
+          로그인
+        </button>
         <div className={styles.search}>
           <span>이메일 찾기</span>
           <div className={styles.line}></div>
