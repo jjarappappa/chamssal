2820b67bffff99066f57e412daa3db818463f986
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-02 20:17:35 +0900

    REFACTOR :: 제목 리팩토링

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Postcode } from "../../components/buttons/Postcode";
 import Header from "../../components/layout/header";
+import { useRecoilState } from "recoil";
+import { signUpInputState } from "../../store/auth/signUpInputAtom";
+import styles from "../../styles/pages/signup.module.scss";
 import Head from "next/head";
+import Title from "../../components/layout/title";
 function SignUp() {
+  const [signUpInput, setSignUpInput] = useRecoilState(signUpInputState);
+  console.log(signUpInput);
   return (
     <div>
       <Head>
         <title>회원가입</title>
       </Head>
       <Header />
-      <Postcode />
+      <div className="background" />
+      <Title>회원가입</Title>
+      {/* <Postcode className={styles.button} /> */}
     </div>
   );
 }
