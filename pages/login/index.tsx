c0bfc08fd2069dc20cfb07d87d119bf9458f4d3a
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-06 20:27:44 +0900

    FIX :: react-hook-form으로 변경

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Header from "../../components/layout/header";
 import Head from "next/head";
 import styles from "../../styles/pages/login.module.scss";
-import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface loginInput {
   email: string;
@@ -10,21 +10,11 @@ interface loginInput {
 }
 
 const Login: NextPage = () => {
-  const [input, setInput] = useState<loginInput>({
-    email: "",
-    password: "",
-  });
-
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-
-    const nextInputs = {
-      ...input,
-      [name]: value,
-    };
-
-    setInput(nextInputs);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
 
   return (
     <div>
@@ -35,27 +25,35 @@ const Login: NextPage = () => {
       <div className="background"></div>
       <div className={styles.login}>
         <h1>로그인</h1>
-        <div className={styles.inputs}>
-          <input
-            type="email"
-            placeholder="이메일"
-            className={styles.input}
-            value={input.email}
-            onChange={(e) => onChange(e)}
-            name="email"
-          />
-          <input
-            type="password"
-            placeholder="비밀번호"
-            className={styles.input}
-            value={input.password}
-            onChange={(e) => onChange(e)}
-            name="password"
-          />
-        </div>
-        <button className={styles.button} onClick={() => console.log(input)}>
-          로그인
-        </button>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <div className={styles.inputs}>
+            <input
+              type="email"
+              placeholder="이메일"
+              className={styles.input}
+              // value={input.email}
+              // onChange={(e) => onChange(e)}
+              {...register("email")}
+              name="email"
+            />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              className={styles.input}
+              // value={input.password}
+              // onChange={(e) => onChange(e)}
+              {...register("password")}
+              name="password"
+            />
+          </div>
+          <button
+            type="submit"
+            className={styles.button}
+            disabled={isSubmitting}
+          >
+            로그인
+          </button>
+        </form>
         <div className={styles.search}>
           <span>이메일 찾기</span>
           <div className={styles.line}></div>
