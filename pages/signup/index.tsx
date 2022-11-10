9e10678c444a8fd657e162005e6d2896dda86668
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-10 23:49:07 +0900

    FIX :: signup react-hook-form으로 변경

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -8,6 +8,7 @@ import Head from "next/head";
 import Title from "../../components/layout/title";
 import { HiOutlineChevronDown } from "react-icons/hi";
 import SignupName from "../../components/signup/SignupName";
+import { useForm } from "react-hook-form";
 import { snackBarState } from "../../store/alert/snackBarAtom";
 
 function SignUp() {
@@ -17,6 +18,7 @@ function SignUp() {
   const [phone, setPhone] = useState(["010", "", ""]);
   const [birth, setBirth] = useState(["", "", ""]);
   const [domain, setDomain] = useState("");
+  const { register, handleSubmit } = useForm();
 
   const changeSignUp = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -74,172 +76,191 @@ function SignUp() {
       /> */}
       <div className="background" />
       <Title>회원가입</Title>
-      <div className={styles.signup}>
-        <div className={styles.item}>
-          <SignupName>이메일</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="text"
-              className={styles.input_middle}
-              name="email"
-              value={signUpInput.email}
-              onChange={(e) => changeSignUp(e)}
-            />
-            <span>@</span>
-            <div className={styles.select}>
+      <form onSubmit={handleSubmit(() => console.log("asdf"))}>
+        <div className={styles.signup}>
+          <div className={styles.item}>
+            <SignupName>이메일</SignupName>
+            <div className={styles.inputs}>
               <input
+                type="text"
                 className={styles.input_middle}
-                placeholder="직접 입력"
-                value={domain}
-                onChange={(e) => setDomain(e.target.value)}
-                onFocus={() => setIsOpenSelect(false)}
+                name="email"
+                value={signUpInput.email}
+                onChange={(e) => changeSignUp(e)}
               />
-              <span
-                className={!isOpenSelect ? styles.arrow_down : styles.arrow_up}
-                onClick={() => setIsOpenSelect((prev) => !prev)}
-              >
-                <HiOutlineChevronDown width={44} height={44} />
-              </span>
+              <span>@</span>
+              <div className={styles.select}>
+                <input
+                  className={styles.input_middle}
+                  placeholder="직접 입력"
+                  value={domain}
+                  onChange={(e) => setDomain(e.target.value)}
+                  onFocus={() => setIsOpenSelect(false)}
+                />
+                <span
+                  className={
+                    !isOpenSelect ? styles.arrow_down : styles.arrow_up
+                  }
+                  onClick={() => setIsOpenSelect((prev) => !prev)}
+                >
+                  <HiOutlineChevronDown width={44} height={44} />
+                </span>
+              </div>
+            </div>
+          </div>
+
+          {isOpenSelect && (
+            <ul>
+              <li onClick={() => changeDomain("naver.com")}>naver.com</li>
+              <li onClick={() => changeDomain("hanmail.net")}>hanmail.net</li>
+              <li onClick={() => changeDomain("gmail.com")}>gmail.com</li>
+              <li onClick={() => changeDomain("yahoo.com")}>yahoo.com</li>
+              <li onClick={() => changeDomain("hotmail.com")}>hotmail.com</li>
+              <li onClick={() => changeDomain("daum.net")}>daum.net</li>
+              <li onClick={() => changeDomain("nate.com")}>nate.com</li>
+            </ul>
+          )}
+
+          <div className={styles.item}>
+            <SignupName>비밀번호</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="password"
+                className={styles.input_long}
+                name="password"
+                value={signUpInput.password}
+                onChange={(e) => changeSignUp(e)}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>비밀번호 확인</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="password"
+                className={styles.input_long}
+                name="confirmPassword"
+                value={signUpInput.confirmPassword}
+                onChange={(e) => changeSignUp(e)}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>전화번호</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="phone"
+                value={phone[0]}
+                onChange={(e) => changePhone(0, e, 3)}
+              />
+              <span>-</span>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="phone"
+                value={phone[1]}
+                onChange={(e) => changePhone(1, e, 4)}
+              />
+              <span>-</span>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="phone"
+                value={phone[2]}
+                onChange={(e) => changePhone(2, e, 4)}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>생년월일</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="birth"
+                value={birth[0]}
+                onChange={(e) => changeBirth(0, e, 4)}
+                placeholder="yyyy"
+              />
+              <span>-</span>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="birth"
+                value={birth[1]}
+                onChange={(e) => changeBirth(1, e, 2)}
+                placeholder="mm"
+              />
+              <span>-</span>
+              <input
+                type="number"
+                className={styles.input_short}
+                name="birth"
+                value={birth[2]}
+                onChange={(e) => changeBirth(2, e, 2)}
+                placeholder="dd"
+                maxLength={4}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>이름</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="name"
+                className={styles.input_long}
+                name="name"
+                value={signUpInput.name}
+                onChange={(e) => changeSignUp(e)}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>닉네임</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="nickname"
+                className={styles.input_long}
+                name="nickname"
+                value={signUpInput.nickname}
+                onChange={(e) => changeSignUp(e)}
+              />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>주소</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="text"
+                className={styles.input_long}
+                name="postcode"
+                value={signUpInput.nickname}
+                onChange={(e) => changeSignUp(e)}
+                readOnly
+                placeholder="우편번호"
+              />
+              <Postcode className={styles.button} />
+            </div>
+          </div>
+          <div className={styles.item}>
+            <SignupName>{""}</SignupName>
+            <div className={styles.inputs}>
+              <input
+                type="nickname"
+                className={styles.input_full}
+                name="nickname"
+                value={signUpInput.nickname}
+                onChange={(e) => changeSignUp(e)}
+                readOnly
+                placeholder="주소"
+              />
+              {/* <Postcode className={styles.button} /> */}
             </div>
           </div>
         </div>
-
-        {isOpenSelect && (
-          <ul>
-            <li onClick={() => changeDomain("naver.com")}>naver.com</li>
-            <li onClick={() => changeDomain("hanmail.net")}>hanmail.net</li>
-            <li onClick={() => changeDomain("gmail.com")}>gmail.com</li>
-            <li onClick={() => changeDomain("yahoo.com")}>yahoo.com</li>
-            <li onClick={() => changeDomain("hotmail.com")}>hotmail.com</li>
-            <li onClick={() => changeDomain("daum.net")}>daum.net</li>
-            <li onClick={() => changeDomain("nate.com")}>nate.com</li>
-          </ul>
-        )}
-
-        <div className={styles.item}>
-          <SignupName>비밀번호</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="password"
-              className={styles.input_long}
-              name="password"
-              value={signUpInput.password}
-              onChange={(e) => changeSignUp(e)}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>비밀번호 확인</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="password"
-              className={styles.input_long}
-              name="confirmPassword"
-              value={signUpInput.confirmPassword}
-              onChange={(e) => changeSignUp(e)}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>전화번호</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="phone"
-              value={phone[0]}
-              onChange={(e) => changePhone(0, e, 3)}
-            />
-            <span>-</span>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="phone"
-              value={phone[1]}
-              onChange={(e) => changePhone(1, e, 4)}
-            />
-            <span>-</span>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="phone"
-              value={phone[2]}
-              onChange={(e) => changePhone(2, e, 4)}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>생년월일</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="birth"
-              value={birth[0]}
-              onChange={(e) => changeBirth(0, e, 4)}
-              placeholder="yyyy"
-            />
-            <span>-</span>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="birth"
-              value={birth[1]}
-              onChange={(e) => changeBirth(1, e, 2)}
-              placeholder="mm"
-            />
-            <span>-</span>
-            <input
-              type="number"
-              className={styles.input_short}
-              name="birth"
-              value={birth[2]}
-              onChange={(e) => changeBirth(2, e, 2)}
-              placeholder="dd"
-              maxLength={4}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>이름</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="name"
-              className={styles.input_long}
-              name="name"
-              value={signUpInput.name}
-              onChange={(e) => changeSignUp(e)}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>닉네임</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="nickname"
-              className={styles.input_long}
-              name="nickname"
-              value={signUpInput.nickname}
-              onChange={(e) => changeSignUp(e)}
-            />
-          </div>
-        </div>
-        <div className={styles.item}>
-          <SignupName>주소</SignupName>
-          <div className={styles.inputs}>
-            <input
-              type="nickname"
-              className={styles.input_long}
-              name="nickname"
-              value={signUpInput.nickname}
-              onChange={(e) => changeSignUp(e)}
-              readOnly
-              placeholder="우편번호"
-            />
-            <Postcode className={styles.button} />
-          </div>
-        </div>
-      </div>
+      </form>
     </div>
   );
 }
