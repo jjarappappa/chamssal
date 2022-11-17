f663cb7a2cb94f3bdddb6ab0545d3471945f20b0
jsm8109jsm <jsm8109jsm@gmail.com>
2022-11-17 16:01:32 +0900

    FEAT :: 강의 보기 페이지 완성 (#7)

--- components/lecture/LectureItem.tsx
+++ components/lecture/LectureItem.tsx
@@ -11,14 +11,16 @@ function LectureItem({ data }: { data: any }) {
           alt="강의 사진"
           sizes="100vw"
           layout="fill"
+          // placeholder="blur"
+          // blurDataURL="/images/logo.png"
         />
       </div>
       <div className={styles.items}>
-        <span>지역</span>
-        <h3>제목</h3>
-        <div>
-          <span>0000-00-00</span>
-          <span>D-33</span>
+        <span className={styles.location}>지역</span>
+        <h3 className={styles.title}>제목</h3>
+        <div className={styles.bottom}>
+          <span className={styles.date}>0000-00-00</span>
+          <span className={styles.d_day}>D-33</span>
         </div>
       </div>
     </div>
