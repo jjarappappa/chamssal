54528b345b6f159e563566b4354f8eb06630dafe
sookyoungwoo <sookyoung2005@gmail.com>
2022-11-30 22:05:36 +0900

    FIX :: 에러 수정 완료 (#18)

--- components/buttons/Mainbutton.tsx
+++ components/buttons/Mainbutton.tsx
@@ -14,7 +14,7 @@ function Mainbutton() {
           <span>신청하기</span>
         </button>
       </Link>
-      <Link href="">
+      <Link href="/classreview">
         <button className={styles.reviewbutton}>
           <div>
             <img src="/images/review.svg" />
