6211c38df34f916ae91a1319c69f2b88475179d8
sookyoungwoo <sookyoung2005@gmail.com>
2022-12-10 23:38:17 +0900

    ADD :: 컴포넌트 수정완료

--- pages/admin/classregist/index.tsx
+++ pages/admin/classregist/index.tsx
@@ -3,48 +3,80 @@ import Sidebar from "../../../components/layout/Sidebar";
 import AdminTitle from "../../../components/layout/AdminTitle";
 import styles from '../../../styles/pages/admin/classregist.module.scss';
 const ClassRegist: NextPage = () => {
-    return(
+    return (
         <>
-            <Sidebar/>
+            <Sidebar />
             <div className="background_admin">
                 <AdminTitle>강의 등록</AdminTitle>
                 <div className={styles.regist_total}>
                     <p className={styles.regist_minip}>
-                        <input placeholder="제목" className={styles.regist_title}/>
-                        </p>  
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>일시</span>
-                        <input />
-                        <input />
-                        <input />
+                        <input placeholder="제목" className={styles.regist_title} />
                     </p>
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>장소</span>
-                        <input className={styles.widthmini}/>
-                    </p>
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>대상</span>
-                        <input className={styles.widthmini}/>
-                    </p>
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>강사</span>
-                        <input className={styles.widthmini}/>
-                    </p>
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>접수기간</span>
-                        <input/>
-                        <input/>
-                        <input/>
-                    </p>
-                    <p className={styles.regist_minip}>
-                        <span className={styles.minititle}>당첨자 발표</span>
-                        <input/>
-                        <input/>
-                        <input/>
-                    </p>
-                    <p className={styles.regist_minip}>
+                    <div className={styles.minititle}>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>일시</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <span className={styles.inputall}>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} />
+                                </span>
+                            </div>
+                        </div>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>장소</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <input className={styles.longinput}/>
+                            </div>
+                        </div>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>대상</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <input  className={styles.longinput}/>
+                            </div>
+                        </div>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>강사</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <input  className={styles.longinput}/>
+                            </div>
+                        </div>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>접수기간</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <span className={styles.inputall}>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} />
+                                </span>
+                            </div>
+                        </div>
+                        <div className={styles.minititle_one}>
+                            <div className={styles.minititle_name}>
+                                <span>당첨자 발표</span>
+                            </div>
+                            <div className={styles.minititle_input}>
+                                <span className={styles.inputall}>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} /><span className={styles.mulgyel}>~</span>
+                                    <input className={styles.widthmini} />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.regist_minip}>
                         <button>+ 이미지 추가</button>
-                    </p>
+                    </div>
                 </div>
             </div>
         </>
