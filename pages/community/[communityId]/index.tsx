e49bb45c99dbf0ddf281347673cccfc91a32b253
jsm8109jsm <jsm8109jsm@gmail.com>
2022-12-11 15:10:57 +0900

    FIX :: community 제목 디자인 수정

--- pages/community/[communityId]/index.tsx
+++ pages/community/[communityId]/index.tsx
@@ -1,26 +1,32 @@
 import { NextPage } from "next";
 import Header from "../../../components/layout/header";
-import CData from '../sanse.json';
-import styles from '../../../styles/pages/detail.module.scss';
+import CData from "../sanse.json";
+import styles from "../../../styles/pages/detail.module.scss";
+import Title from "../../../components/layout/title";
 const Detail: NextPage = () => {
-    return (
+  return (
+    <>
+      <Header />
+      <div className="background">
         <div className={styles.Detailbody}>
-            <Header />
-            <div className={styles.Detailtit}>
-                <div className={styles.Detaildetail}>
-                    <div className={styles.Detailtop}>
-                        <div className={styles.Detailtitle}>{CData.title}</div>
-                        <div className={styles.Detailright}>
-                            <span className={styles.Detailname}>{CData.user.name}</span>
-                            <span className={styles.Detailbirth}>{CData.user.birthday}</span>
-                        </div>
-                    </div>
-                    <p className={styles.Detailbottom}>
-                        {CData.content}
-                    </p>
+          <div className={styles.Detailtit}>
+            <div className={styles.Detaildetail}>
+              <div className={styles.Detailtop}>
+                {/* <div className={styles.Detailtitle}>{CData.title}</div> */}
+                <Title>{CData.title}</Title>
+                <div className={styles.Detailright}>
+                  <span className={styles.Detailname}>{CData.user.name}</span>
+                  <span className={styles.Detailbirth}>
+                    {CData.user.birthday}
+                  </span>
                 </div>
+              </div>
+              <p className={styles.Detailbottom}>{CData.content}</p>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 export default Detail;
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,30 +1,29 @@
 import { NextPage } from "next";
 import Information from "../../components/Information/community";
-import ReviewData from './reviewd.json';
+import ReviewData from "./reviewd.json";
 import Header from "../../components/layout/header";
-import styles from '../../styles/pages/review.module.scss';
+import styles from "../../styles/pages/review.module.scss";
 import Link from "next/link";
+import Title from "../../components/layout/title";
 const Review: NextPage = () => {
-    return (
-        <div className={styles.reviewall}>
-            <Header />
-            <div className={styles.reviewgang}>
-                <div>
-                    <div className={styles.reviewtxt}><p>강의 후기</p></div>
-                    <div>
-                        {
-                            ReviewData.data.map(r => (
-                                <Information
-                                    title={r.title}
-                                    name={r.userName}
-                                    day={r.createdAt}
-                                />
-                            ))
-                        }
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.reviewall}>
+      <Header />
+      <div className={styles.reviewgang}>
+        <div>
+          <Title>강의 후기</Title>
+          <div className={styles.reviewItem}>
+            {ReviewData.data.map((r) => (
+              <Information
+                title={r.title}
+                name={r.userName}
+                day={r.createdAt}
+              />
+            ))}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default Review;
